e1be0323db16430e13db71040f627f0125186383
JuanMiguelD <juandimatemiguel@gmail.com>
2024-10-29 13:44:39 -0500

    first commit

---
+++ src/main/java/com/proyecto/backfinal/BackfinalApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.backfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackfinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/backfinal/BackfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.backfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
