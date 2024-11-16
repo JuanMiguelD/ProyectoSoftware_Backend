c95b401c1311b1a8514df6041b48ae8457da9cfc
JuanMiguelD <juandimatemiguel@gmail.com>
2024-11-16 16:42:06 -0500

    Corrección en eliminar libro

--- src/main/java/com/proyecto/backfinal/controllers/BookController.java
+++ src/main/java/com/proyecto/backfinal/controllers/BookController.java
@@ -9,9 +9,9 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -78,8 +78,8 @@ public class BookController {
     }
 
 
-    @PostMapping("DeleteBook")
-    public ResponseEntity<?> deleteBook(@RequestBody long id){
+    @DeleteMapping("DeleteBook")
+    public ResponseEntity<?> deleteBook(@RequestParam long id){
 
         bookService.deleteBook(id);
 
